7325392d7a3e90c69b8992c36c93e78d9f0a29f0
kelwinfc <kelwinfc@caracas.(none)>
2014-01-13 10:43:13 +0000

    No-op behaviour.

--- lib/behaviour.hpp
+++ lib/behaviour.hpp
@@ -5,14 +5,26 @@
 
 #include "graph.hpp"
 #include "utils.hpp"
+#include "agent.hpp"
 
 using namespace std;
+
+class agent;
 
 //TODO define the basic functions to define a behaviour
 class behaviour {
     
     public:
         behaviour();
+        
+        virtual int next_step(agent* a){ return -1; }
+};
+
+class noop {
+    
+    public:
+        noop();
+        virtual int next_step(agent* a);
 };
 
 #endif
--- src/behaviour.cpp
+++ src/behaviour.cpp
@@ -1,1 +1,11 @@
 #include "behaviour.hpp"
+
+noop::noop()
+{
+    
+}
+
+int noop::next_step(agent* a)
+{
+    return a->get_current_position();
+}
