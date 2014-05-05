6129f436455209328040fd836042b3a4915511a5
kelwinfc <kelwinfc@caracas.(none)>
2014-05-05 17:04:33 +0100

    Calculados los puntos de bifurcación del Medial axis.

--- utils/get_navigation_mesh.py
+++ utils/get_navigation_mesh.py
@@ -4,83 +4,85 @@ import os
 
 import matplotlib.delaunay as triang
 from skimage import morphology
+
 import numpy as np
 import cv2
 
-def draw_corners(img, corners, v=0.5):
-    dst = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-    dilated_corners = cv2.dilate(corners, None)
-    dilated_corners = cv2.dilate(dilated_corners, None)
-    
-    dst[dilated_corners >= v * dilated_corners.max()] = [0,0,255]
-    
-    cv2.imshow('corners',dst)
+def get_neighbor(img, i, j, (di, dj)):
+    rows, cols = img.shape
+    ni = i + di
+    nj = j + dj
+    if 0 <= ni and ni < rows and 0 <= nj and nj < cols:
+        return (ni,nj)
+    return None
 
-def get_points(corners, v=0.5):
-    def to_grid(p):
-        xs = list(set([x for (x, _) in p]))
-        ys = list(set([y for (_, y) in p]))
-        xs.sort()
-        ys.sort()
+def belongs_to_skeleton(img, p):
+    if p is None:
+        return True
+
+    (i,j) = p
+
+    if img[i][j] == 255:
+        return True
+    
+    return False
+
+def count_neighbors(img, i, j):
+    rows, cols = img.shape
+    ret = 0
+    
+    """
+    -1,-1 -1,+0, -1,+1
+    +0,-1 +0,+0, +0,+1
+    +1,-1 +1,+0, +1,+1
+    """
+    neighbors = [(-1,-1),(0,-1),(1,-1),(1,0),(1,1),(0,1),(-1,1),(-1,0)]
+    
+    for index, n in enumerate(neighbors):
+        prev_n = get_neighbor(img, i, j, n)
+        current_n = get_neighbor(img, i, j,
+                                 neighbors[(index + 1) % len(neighbors)])
         
-        print xs
-        print ys
+        if prev_n is None or current_n is None:
+            continue
         
-        p = []
-        for idx, x in enumerate(xs):
-            if idx > 0 and abs(xs[idx] - xs[idx-1]) < 5:
+        if not belongs_to_skeleton(img, prev_n) and \
+               belongs_to_skeleton(img, current_n):
+            ret += 1
+    
+    return ret
+
+def get_branching_points(img):    
+    points = []
+    
+    rows, cols = img.shape
+    
+    for i in xrange(rows):
+        for j in xrange(cols):
+            if img[i][j] == 0:
                 continue
             
-            for idy, y in enumerate(ys):
-                if idy > 0 and abs(ys[idy] - ys[idy-1]) < 5:
-                    continue
-                
-                p.append([y, x])
-        return p
+            nn = count_neighbors(img, i, j)
+            if nn > 2 or nn == 1:
+                points.append((j,i))
+    print len(points), "branching points"
+    return points
+
+def draw_branching_points(img, p):
+    dst = cv2.merge([img, img, img])#np.zeros(img.shape, np.uint)
+    dst = 255 - dst
     
-    max_v = corners.max()
-    rows, cols = corners.shape
-    ret = [[0, 0], [rows - 1,0], [0, cols - 1], [rows - 1, cols - 1]]
-    
-    for idx, x in enumerate(corners):
-        for idy, y in enumerate(x):
-            if y >= v * max_v:
-                ret.append([idx, idy])
-    
-    return to_grid(ret)
-
-def flip_points(points, rows, cols):
-    return [ [rows - x[0], cols - x[1]] for x in points]
-
-def draw_triangles(img, points, tri):
-
-    dst = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-    
-    for t in tri:
-        t = np.array([points[t[0]], points[t[1]], points[t[2]]])
-        cv2.polylines(dst, [t], True, (255,0,0))
-    cv2.imshow("triangles", dst)
-    
-def medial_axis(img):
-    rows, cols = img.shape
-    for x in xrange(rows):
-        img[x][0] = 0
-        img[x][cols-1] = 0
-    for x in xrange(cols):
-        img[0][x] = 0
-        img[rows-1][x] = 0
-
-
-    skel, distance = morphology.medial_axis(img, return_distance=True)
-    skel = [ [ 255 if y else 0 for y in x] for x in skel]
-    skel = np.array(skel)
-
-    return skel, distance
+    color = (0,0,255)
+    for c in p:
+        #dst[c[1]][c[0]] = color(0,0,255)
+        cv2.circle(dst, c, 5, color, 2)
+        
+    return dst
 
 if len(argv) < 2:
     sys.exit(-1)
 
-path = "results/medial_axis/" + argv[1].split("/")[-1].split(".")[0] + "/"
+path = "results/medial_axis/" + argv[2] + "/"
 print "Output path:", path
 
 try:
@@ -90,21 +92,13 @@ except OSError as exc:
         pass
 
 filename = argv[1]
-img = cv2.imread(filename)
-cv2.imwrite(path + 'src.jpg', img)
+ma = cv2.imread(filename, 0)
+_, ma = cv2.threshold(ma, 100, 255, cv2.THRESH_BINARY)
 
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-gray = cv2.threshold(gray, 20, 255, cv2.THRESH_BINARY)[1]
+p = get_branching_points(ma)
 
-kernel = np.ones((3,3), np.uint8)
-gray = cv2.erode(gray, kernel, iterations = 1)
+bp_img = draw_branching_points(ma, p)
 
-cv2.imwrite(path + 'binarized.jpg', gray)
-print "Image binarized"
+cv2.imwrite(path + 'branching_points.jpg',  bp_img)
 
-ma, distance = medial_axis(gray)
-
-cv2.imwrite(path + 'medial_axis.jpg',  ma)
-cv2.imwrite(path + 'distance_transform.jpg',  distance)
-
-print "Medial axis computed"
+print "Branching points computed"
